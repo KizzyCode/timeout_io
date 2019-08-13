d915dd74c79f03948c020c3a1770275082be8346
Keziah Biermann <development@kizzycode.de>
2019-08-13 02:11:48 +0200

    Updated style

--- src/acceptor.rs
+++ src/acceptor.rs
@@ -1,20 +1,24 @@
 use crate::{ TimeoutIoError, InstantExt, WaitForEvent, EventMask };
-use std::{ io::Result as IoResult, time::{ Duration, Instant }, net::{ TcpListener, TcpStream } };
+use std::{
+	io,
+	time::{ Duration, Instant },
+	net::{ TcpListener, TcpStream }
+};
 
 
 /// A private trait wrapping the standard library's acceptors
 #[doc(hidden)]
 pub trait StdAcceptor<T> where Self: WaitForEvent {
-	fn accept(&self) -> IoResult<T>;
+	fn accept(&self) -> Result<T, io::Error>;
 }
 impl StdAcceptor<TcpStream> for TcpListener {
-	fn accept(&self) -> IoResult<TcpStream> {
+	fn accept(&self) -> Result<TcpStream, io::Error> {
 		Ok(TcpListener::accept(self)?.0)
 	}
 }
 #[cfg(unix)]
 impl StdAcceptor<::std::os::unix::net::UnixStream> for ::std::os::unix::net::UnixListener {
-	fn accept(&self) -> IoResult<::std::os::unix::net::UnixStream> {
+	fn accept(&self) -> Result<::std::os::unix::net::UnixStream, io::Error> {
 		Ok(::std::os::unix::net::UnixListener::accept(self)?.0)
 	}
 }
@@ -33,11 +37,11 @@ impl<U, T: StdAcceptor<U> + WaitForEvent> Acceptor<U> for T {
 		// Make the socket non-blocking
 		self.set_blocking_mode(false)?;
 		
-		// Compute timeout-point and try to accept once until the timeout occurred
-		let timeout_point = Instant::now() + timeout;
+		// Compute deadline and try to accept once until the timeout occurred
+		let deadline = Instant::now() + timeout;
 		loop {
 			// Wait for read-event
-			self.wait_for_event(EventMask::new_r(), timeout_point.remaining())?;
+			self.wait_for_event(EventMask::new_r(), deadline.remaining())?;
 			
 			// Accept connection
 			match StdAcceptor::accept(self) {
